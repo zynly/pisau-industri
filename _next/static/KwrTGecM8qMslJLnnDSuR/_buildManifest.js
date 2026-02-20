5b6a2c8b3806bfe4e38ec5b2985246903eae396b
M Zeydan Aly Aqshol Jihad <107059561+zynly@users.noreply.github.com>
2026-02-20 15:12:18 +0700

    Updates

---
+++ _next/static/KwrTGecM8qMslJLnnDSuR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pisau-industri//_next/:path+",
+        "destination": "/pisau-industri/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
